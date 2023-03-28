267e8db1873bf35802eb3d9e355e27b0d016b8e5
Alex <cikosalex@gmail.com>
2023-03-28 12:12:51 +1030

    ready to deploy

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -22,33 +22,35 @@ const Login = () => {
 
   // JSX for the login page
   return (
-    <form
-      className="login"
-      onSubmit={handleSubmit}
-    >
-      <h3>Login</h3>
+    <div className="LoginPage">
+      <form
+        className="login"
+        onSubmit={handleSubmit}
+      >
+        <h3>Login</h3>
 
-      {/* Setting the email, so it can be in the state */}
-      <label>Email:</label>
-      <input
-        type="email"
-        // This how you the put the value into the state
-        onChange={e => setEmail(e.target.value)}
-        value={email}
-      />
-      <label>Password:</label>
-      <input
-        type="password"
-        onChange={e => setPassword(e.target.value)}
-        value={password}
-      />
+        {/* Setting the email, so it can be in the state */}
+        <label>Email:</label>
+        <input
+          type="email"
+          // This how you the put the value into the state
+          onChange={e => setEmail(e.target.value)}
+          value={email}
+        />
+        <label>Password:</label>
+        <input
+          type="password"
+          onChange={e => setPassword(e.target.value)}
+          value={password}
+        />
 
-      {/* If the isloading is true, the button is disabled */}
-      <button disabled={isLoading}>Login</button>
+        {/* If the isloading is true, the button is disabled */}
+        <button disabled={isLoading}>Login</button>
 
-      {/* If error is true, it returns a div, with the error message */}
-      {error && <div className="error">{error}</div>}
-    </form>
+        {/* If error is true, it returns a div, with the error message */}
+        {error && <div className="error">{error}</div>}
+      </form>
+    </div>
   )
 }
 
